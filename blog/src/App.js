10e54f5b62fb302287495650807e5822768925e7
yeinHan <goregore8637@naver.com>
2023-03-23 01:06:57 +0900

    state 공부

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,41 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, 글제목변경] = useState(['남자 코트 추천', '엽떡 맛집', '닭한마리 맛집']);
+
+  let posts = '강남 고기 맛집'
+
+  let blogStyle = {color: 'blue', fontSize: '30px'}
+
+  function test() {
+    return 100
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello 아임 마드모아젤  = 재형 여친 임다 22 33
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div style={blogStyle}>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3> {title[0]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[2]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
